c84bccdbafb487174a5031c6c9e84c5a58f10f86
claajulian <clajulian86@gmail.com>
2022-06-02 08:51:48 -0300

    modificacion NavBar

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <NavBar/>
+        <h2>RETRO CLUB VHS</h2>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
