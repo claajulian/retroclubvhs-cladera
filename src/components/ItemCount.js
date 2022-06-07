5f9a6fef257e8e236ecccd22364f7fdd9bc185e9
claajulian <clajulian86@gmail.com>
2022-06-06 22:01:41 -0300

    ÂSe agregó boton contadorÂ

---
+++ src/components/ItemCount.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+
+function ItemCount({ stock, initial }) {
+
+    const [cantidad, setCantidad] = useState(initial)
+
+    function sumar() {
+        if (cantidad < stock) setCantidad(cantidad + 1)
+    }
+
+    function restar() {
+        if (cantidad > initial) setCantidad(cantidad - 1)
+    }
+
+    function agregarCarrito() {
+        console.log(`Se agregó al carrito ${cantidad} unidades`);
+        alert(`Se agregó al carrito ${cantidad} unidades`)
+    }
+
+    return (
+        <div>
+            <button onClick={restar}>-</button>
+            <span>{cantidad}</span>
+            <button onClick={sumar}>+</button>
+            <button onClick={agregarCarrito}>Agregar al carrito</button>
+        </div>
+    )
+}
+
+export default ItemCount
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import ItemCount from './ItemCount'
 
 export default function ItemListContainer(props) {
   return (
-      
-    <div>{props.greeting}</div>
+
+    <div>{props.greeting}
+    <br></br>
+      <ItemCount stock={5} initial={1} />
+    </div>
   )
 }
